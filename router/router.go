ed3aeabf386f14f9e99dc673dcd1c38bb06064d9
althaafka <althaafka004@gmail.com>
2024-06-19 15:27:01 +0700

    fix cors origin

--- router/router.go
+++ router/router.go
@@ -8,16 +8,32 @@ import (
 	"github.com/althaafka/alk-proj-be.git/middlewares"
 )
 
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+
 func SetupRouter() {
-	http.HandleFunc("/users/register", usercontroller.Register)
-	http.HandleFunc("/users/login", usercontroller.Login)
+	http.Handle("/users/register", corsMiddleware(http.HandlerFunc(usercontroller.Register)))
+	http.Handle("/users/login", corsMiddleware(http.HandlerFunc(usercontroller.Login)))
 
-	http.HandleFunc("/articles", articlecontroller.GetArticles)
-	http.Handle("/articles/create", middlewares.AuthMiddleware(http.HandlerFunc(articlecontroller.CreateArticle)))
-    http.Handle("/articles/edit", middlewares.AuthMiddleware(http.HandlerFunc(articlecontroller.EditArticle)))
-    http.Handle("/articles/delete", middlewares.AuthMiddleware(http.HandlerFunc(articlecontroller.DeleteArticle)))
+	http.Handle("/articles", corsMiddleware(http.HandlerFunc(articlecontroller.GetArticles)))
+	http.Handle("/articles/create", corsMiddleware(middlewares.AuthMiddleware(http.HandlerFunc(articlecontroller.CreateArticle))))
+    http.Handle("/articles/edit", corsMiddleware(middlewares.AuthMiddleware(http.HandlerFunc(articlecontroller.EditArticle))))
+    http.Handle("/articles/delete", corsMiddleware(middlewares.AuthMiddleware(http.HandlerFunc(articlecontroller.DeleteArticle))))
 
-	log.Println("Server started at localhost:8080")
+	log.Println("Server started on localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
