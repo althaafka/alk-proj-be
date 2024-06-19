0341212a6971a3da761a13797b5942140760d807
althaafka <althaafka004@gmail.com>
2024-06-19 11:59:45 +0700

    init go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/althaafka/alk-proj-be.git/database"
+	"github.com/althaafka/alk-proj-be.git/router"
+)
+
+func main() {
+	database.Connect()
+
+	router.SetupRouter()
+}
