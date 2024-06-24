48f8c2fbd021923d4150116f9192e64f2ba451b8
althaafka <althaafka004@gmail.com>
2024-06-24 13:56:36 +0700

    chhore env var

--- database/setup.go
+++ database/setup.go
@@ -1,14 +1,34 @@
 package database
+
 import (
-	"gorm.io/gorm"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"github.com/althaafka/alk-proj-be.git/models"
 )
 
-var DB *gorm.DB;
+var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=qwerty dbname=alkademi port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+	timezone := os.Getenv("DB_TIMEZONE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		host, user, password, dbname, port, sslmode, timezone)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.24.0
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.10
